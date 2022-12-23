dc61a8aaf105dee9a3568e47169fadaf4f167af7
Juan bigorra <juanchoibf@gmail.com>
2022-12-23 18:05:28 +0100

    Find one overlapping pair

--- src/camp_cleanup.py
+++ src/camp_cleanup.py
@@ -2,4 +2,11 @@ from typing import Tuple
 
 
 def find_overlapping_pairs_from(pairs):
+    for set_of_pairs in pairs:
+        pair_1 = set_of_pairs[0]
+        pair_2 = set_of_pairs[1]
+        print(pair_1, pair_2)
+        if pair_1[0] < pair_2[0] < pair_1[1]:
+            return 1
+
     return 0
--- tests/test_camp_cleanup.py
+++ tests/test_camp_cleanup.py
@@ -1,5 +1,13 @@
 from src.camp_cleanup import find_overlapping_pairs_from
 
+# """
+# 2-4,6-8
+# 2-3,4-5
+# 5-7,7-9
+# 2-8,3-7
+# 6-6,4-6
+# 2-6,4-8
+# """
 
 class TestCampCleanup:
     def test_no_overlapping_pairs_found(self):
@@ -14,7 +22,16 @@ class TestCampCleanup:
         assert result == 0
 
     def test_find_one_overlapping_pair(self):
-        assert True is False
+        pairs = (
+            ((2, 4), (6, 8)),
+            ((2, 3), (4, 5)),
+            ((5, 7), (7, 9)),
+            ((2, 8), (3, 7)),
+        )
+
+        result = find_overlapping_pairs_from(pairs)
+
+        assert result == 1
 
     def test_find_two_overlapping_pairs(self):
         assert True is False
