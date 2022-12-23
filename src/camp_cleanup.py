38eb346fab0773632f4689e160ff2325ff37a3e6
Juan bigorra <juanchoibf@gmail.com>
2022-12-23 19:06:41 +0100

    Test/Implement number within range and pair within range

--- src/camp_cleanup.py
+++ src/camp_cleanup.py
@@ -1,6 +1,16 @@
 from typing import Tuple
 
 
+def exist_within_range(range_: Tuple[int, int], number: int) -> bool:
+    return True if range_[0] <= number <= range_[1] else False
+
+
+def pair_exist_within_range(range_: Tuple[int, int], pair: Tuple[int, int]) -> bool:
+    if exist_within_range(range_=range_, number=pair[0]) and exist_within_range(range_=range_, number=pair[1]):
+        return True
+
+    return False
+
 def find_overlapping_pairs_from(pairs):
     for set_of_pairs in pairs:
         pair_1 = set_of_pairs[0]
--- tests/test_camp_cleanup.py
+++ tests/test_camp_cleanup.py
@@ -1,4 +1,9 @@
-from src.camp_cleanup import find_overlapping_pairs_from
+from typing import Tuple
+
+import pytest
+
+from src.camp_cleanup import find_overlapping_pairs_from, exist_within_range, pair_exist_within_range
+
 
 # """
 # 2-4,6-8
@@ -8,6 +13,46 @@ from src.camp_cleanup import find_overlapping_pairs_from
 # 6-6,4-6
 # 2-6,4-8
 # """
+
+class TestNumberExistWithinRange:
+    def test_number_exist_within_range(self):
+        pair1 = (2, 8)
+
+        result = exist_within_range(range_=pair1, number=3)
+
+        assert result is True
+
+    def test_number_does_not_exist_within_range(self):
+        pair1 = (2, 8)
+
+        result = exist_within_range(range_=pair1, number=1)
+
+        assert result is False
+
+
+class TestPairExistWithinARange:
+    @pytest.mark.parametrize("pair_out_of_range", [
+        (0, 1),
+        (1, 2),
+        (8, 12),
+        (9, 12),
+    ])
+    def test_pair_does_not_exist_within_range_given_first_number_is_out_of_range(
+        self, pair_out_of_range: Tuple[int, int]
+    ):
+        range_ = (2, 8)
+        result = pair_exist_within_range(range_=range_, pair=pair_out_of_range)
+
+        assert result is False
+
+    def test_pair_exist_within_range(self):
+        range = (2, 8)
+        pair2 = (3, 7)
+
+        result = pair_exist_within_range(range_=range, pair=pair2)
+
+        assert result is True
+
 
 class TestCampCleanup:
     def test_no_overlapping_pairs_found(self):
